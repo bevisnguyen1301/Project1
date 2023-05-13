ee715f0744632c1f293f4d7ef9ed42738e35c11b
bevisnguyen1301 <132751104+bevisnguyen1301@users.noreply.github.com>
2023-05-14 01:17:04 +0700

    Add files via upload

---
+++ Head.java
@@ -0,0 +1,13 @@
+package project;
+
+public class Head {
+	public static void main(String[] args) {
+		CongTru m = new CongTru();
+		NhanChia n = new NhanChia();
+		m.nhap();
+		m.tinh();
+		n.nhap();
+		n.tinh();
+		
+	}
+}
